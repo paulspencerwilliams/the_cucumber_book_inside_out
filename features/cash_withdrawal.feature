add9003e2eef0a7ddc8fe79fc4744e07438d387b
Paul Williams <paulswilliams@me.com>
2012-10-15 17:25:22 +0100

    introduced the world

--- features/cash_withdrawal.feature
+++ features/cash_withdrawal.feature
@@ -1,5 +1,5 @@
 Feature: Cash Withdrawal
 	Scenario: Successful withdrawal from an account in credit
 	Given i have deposited $100 in my account
-	When I request $20
+	When I withdraw $20
 	Then $20 should be dispensed
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -8,14 +8,31 @@ class Account
 		@balance
 	end
 end
-Given /^i have deposited \$(\d+) in my account$/ do |amount|
-	my_account = Account.new
-	my_account.deposit(amount.to_i)  
-	my_account.balance.should eq(amount.to_i)
+
+class Teller
+	def withdraw_from(account, amount)
+	end
 end
 
-When /^I request \$(\d+)$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+module KnowsMyAccount
+	def my_account
+		@my_account ||= Account.new
+	end
+end
+
+World(KnowsMyAccount)
+
+CAPTURE_A_NUMBER = Transform /^\$(\d+)$/ do | number |
+	number.to_i
+end
+Given /^i have deposited (#{CAPTURE_A_NUMBER}) in my account$/ do |amount|
+	my_account.deposit(amount)  
+	my_account.balance.should eq(amount)
+end
+
+When /^I withdraw (#{CAPTURE_A_NUMBER})/ do |amount|
+  teller = Teller.new
+  teller.withdraw_from(my_account, amount)
 end
 
 Then /^\$(\d+) should be dispensed$/ do |arg1|

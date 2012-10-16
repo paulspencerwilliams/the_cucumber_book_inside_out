79245ee193cdb47f168a80557e0b9839f2971f24
Paul Williams <paulswilliams@me.com>
2012-10-16 20:59:25 +0100

    fixed async issues through sampling

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,9 +7,9 @@ When /^I withdraw (#{CAPTURE_A_NUMBER})/ do |amount|
 end
 
 Then /^(#{CAPTURE_A_NUMBER}) should be dispensed/ do |amount|
-  cash_slot.contents.should == amount
+  cash_slot.contents.should eq(amount) 
 end
 
 And /^the balance of my account should be (#{CAPTURE_A_NUMBER})/ do |amount|
-  my_account.balance.should eq(amount)
+  eventually { my_account.balance.should eq(amount)}
 end
